7f472ed8f8cbeb7ae435f346e5d094a87a7b6c82
Saurabh <samittal@deloitte.com>
2020-05-05 08:33:29 +0530

    Day 1 excercies

---
+++ D1E2.py
@@ -0,0 +1,12 @@
+# change this code
+mystring = "hello"
+myfloat = 1.0
+myint = 2020
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 1.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 2020:
+    print("Integer: %d" % myint)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PythonLearning
+D1E1 Print the following: -  “Hello ATS Team” 
+
+D1E2 Create a string, integer and float. Print String only if it is “hello”, print integer if it is 2020 and print float if it is 1.0 
+
+D1E3 Input two numbers and perform addition, multiplication 
